c3f57124712f3260bdfbd98be888759462d64053
fakepixels <tinaxhe@gmail.com>
2025-01-23 12:50:08 -0500

    Refactor FundCardComponents to improve text alignment styles

--- app/components/Fund.tsx
+++ app/components/Fund.tsx
@@ -2,7 +2,7 @@ import { FundCard } from '@coinbase/onchainkit/fund';
 
 export default function FundCardComponents() {
   return (
-    <div className="p-4">
+    <div className="p-4 [&_*]:text-left [&_div]:text-left [&_p]:text-left">
       <FundCard assetSymbol="ETH" country="US" />
     </div>
   );
