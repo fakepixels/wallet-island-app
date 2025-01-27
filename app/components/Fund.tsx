4936b6feebf690de97a08c92a24a819660dfc489
fakepixels <tinaxhe@gmail.com>
2025-01-27 16:28:25 -0500

    Update OnchainKit to v0.36.8 and add preset amount inputs to FundCard

--- app/components/Fund.tsx
+++ app/components/Fund.tsx
@@ -1,9 +1,15 @@
 import { FundCard } from '@coinbase/onchainkit/fund';
 
 export default function FundCardComponents() {
+  const presetAmountInputs = ['5', '10', '20'] as const;
+
   return (
     <div className="p-4 [&_*]:text-left [&_div]:text-left [&_p]:text-left">
-      <FundCard assetSymbol="ETH" country="US" />
+      <FundCard 
+        assetSymbol="ETH" 
+        country="US"
+        presetAmountInputs={presetAmountInputs}
+      />
     </div>
   );
 }
